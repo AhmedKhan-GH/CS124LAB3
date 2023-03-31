d7c9b6f49767e36c31290f683dc2ff9eed61ff3f
hydraxl <adam.freed123@gmail.com>
2023-03-31 15:38:02 -0700

    hashmap

--- hashmap.cpp
+++ hashmap.cpp
@@ -1,1 +1,45 @@
 #include "hashmap.hpp"
+#include <utility>
+#include <stdexcept>
+
+// hash function
+int hash(std::string cipher) {
+    int result = 0;
+    int multiplier = 1;
+    for (char c : cipher) {
+        if (c == '1') { result += multiplier; }
+
+        // error handling
+        else if (c != '0') {
+            throw std::invalid_argument("Cipher includes chars other than 1 or 0");
+        }
+
+        // double the value of the next digit
+        multiplier *= 2;
+    }
+
+    // fit to hash size
+    result %= HASH_SIZE;
+    return result;
+}
+
+// adds a new element to the hash map
+void hashmap::insert(std::pair<char, std::string> data) {
+    int index = hash(data.second);
+    hash_node* new_node = new Hash_node;
+    new_node->data = data;
+    new_node->next = table[index];
+    table[index] = new_node;
+}
+
+// finds the char that is represented by a given code
+char hashmap::find(std::string cipher) const {
+    int index = hash(cipher);
+    hash_node* current = table[index];
+    while (current != nullptr) {
+        if (current->data.second == cipher) { return current->data.first; }
+        current = current->next;
+    }
+
+    return ' '; // failed to find char
+}
--- hashmap.hpp
+++ hashmap.hpp
@@ -1,10 +1,25 @@
 #ifndef HASHMAP_HPP
 #define HASHMAP_HPP
+#include <utility>
+#include <string>
+
+const int HASH_SIZE = 32; // size of hashmap
+
+// Node for sheparate chaining
+struct ash_node {
+    std::pair<char, std::string> data; // code is represented as a string of 1s and 0s
+    hash_node* next = nullptr;
+};
 
 class hashmap
 {
 public:
+    void insert(std::pair<char, std::string> data); // adds a new element to the hash map
+    char find(std::string cipher) const; // finds the char that is represented by a given code
+
 private:
+    hash_node* table[HASH_SIZE];
+    int hash(std::string cipher); // hash function
 };
 
 #endif //HASHMAP_HPP
