4d2a1b0ce3d827288999b88de6b02b65f3019a1f
Ahmed <emailahmedebadkhan@gmail.com>
2023-04-01 11:35:41 -0700

    starting parser class, updating gitingore

---
+++ parser.hpp
@@ -0,0 +1,14 @@
+#ifndef PARSER_HPP
+#define PARSER_HPP
+#include <fstream>
+class parser
+{
+public:
+	parser();
+	static does_this_file_exist(std::string file_name)
+	{
+
+	}
+private:
+};
+#endif //PARSER_HPP
