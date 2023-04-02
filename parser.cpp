c8b6478b26e22501aaf00b00d0bdc3b9f690194d
Ahmed <emailahmedebadkhan@gmail.com>
2023-04-01 18:34:47 -0700

    added function rudaments for parser class

--- parser.cpp
+++ parser.cpp
@@ -1,2 +1,32 @@
 #include "parser.hpp"
 
+bool parser::does_this_file_exist(std::string file_name)
+{
+	std::ifstream input_filestream(file_name);
+	return file.good();
+}
+
+parser::parser(std::string file_name) : file_name(file_name)
+{
+
+}
+
+std::vector<std::pair<char, std::string>> parser::parse_cypher()
+{
+
+}
+
+std::vector<<std::string> parse_encryption
+{
+
+}
+
+std::vector<<std::string> parser::parse_plaintext()
+{
+
+}
+
+bool parser::is_this_file_encrypted(std::string file_name)
+{
+
+}
--- parser.hpp
+++ parser.hpp
@@ -1,14 +1,18 @@
 #ifndef PARSER_HPP
 #define PARSER_HPP
 #include <fstream>
+#include <vector>
+#include <utility>
 class parser
 {
 public:
-	parser();
-	static does_this_file_exist(std::string file_name)
-	{
-
-	}
+	parser(std::string file_name);
+	static bool does_this_file_exist(std::string file_name);
+	std::vector<std::pair<char, std::string>> parse_cypher();
+	std::vector<<std::string> parse_encryption;
+	std::vector<<std::string> parse_plaintext;
+	static bool is_this_file_encrypted(std::string file_name);
 private:
+	std::string file_name;
 };
 #endif //PARSER_HPP
